0bc72116ef4fe8f8ffee4cfa58a29c123ff822aa
Patrick Urbankowski <purbankowski@gmail.com>
2020-06-25 19:32:04 -0400

    added initial products to seeds10.sql

---
+++ seeds10.sql
@@ -0,0 +1,31 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("gray thermal sweatshirt", "Clothing", 315.00, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("khaki thermal sweatshirt", "Clothing", 315.00, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("black denim jacket", "Clothing", 1075.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("gray hoodie", "Clothing", 475.00, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("yellow cap", "Accesories", 120.00, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("gold bracelet", "Accesories", 120.00, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("denim tote", "Bags", 310.00, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("black duffle bag", "Bags", 3590.00, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("gray 990v5 sneakers", "Shoes", 175.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("black derbys", "Shoes", 360.00, 15);
