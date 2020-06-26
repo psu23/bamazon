1082d49994fd0617c36e689f2f72b9ab8f0fd213
Patrick Urbankowski <purbankowski@gmail.com>
2020-06-25 20:15:26 -0400

    table of inventory shown to user

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "replace with your password",
+    password: "password",
     database: "bamazon"
 });
 
@@ -16,5 +17,20 @@ connection.connect(function(err){
 });
 
 function bamazonCustomer() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
 
+        var inventoryTable = new Table({
+            head: ["ID", "Item", "Department", "Price", "Quantity"],
+            colWidths: [10, 50, 20, 10, 10],
+        });
+
+        for (var i=0; i<res.length; i++){
+            inventoryTable.push([res[i].id, res[i].product_name, res[i].department_name, 
+                parseFloat(res[i].price), res[i].stock_quantity]);
+        }
+
+        console.log(inventoryTable.toString());
+
+    })
 }
